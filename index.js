bf00d0450b41ae1c3c8426842a791bf14071eaf1
TimChern111 <66830325+TimChern111@users.noreply.github.com>
2020-09-30 16:53:15 +0300

    Final Timer

--- index.js
+++ index.js
@@ -1,86 +1,51 @@
-let timer = document.querySelector('#timer-1');
-let days = document.querySelector('.value[data-value="days"]');
-let hours = document.querySelector('.value[data-value="hours"]');
-let mins = document.querySelector('.value[data-value="mins"]');
-let secs = document.querySelector('.value[data-value="secs"]');
 
-class CountdownTimer {
+const ref = {
+  day: document.querySelector(".value[data-value='days']"),
+  hour: document.querySelector(".value[data-value='hours']"),
+  min: document.querySelector(".value[data-value='mins']"),
+  secs: document.querySelector(".value[data-value='secs']"),
+  timer: document.querySelector('#timer-1')
+};
+
+class CountdownTimer{
   constructor({selector, targetDate}){
     this.selector = selector;
-    this.targetDate = targetDate
+    this.targetDate = targetDate;
   }
 
+  updateClockface() {
+    setInterval(() => {
+      const startTime = new Date().getTime();
+      let time = this.targetDate - startTime;
+
+      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+
+      ref.day.textContent = `${days}`;
+      ref.hour.textContent = `${hours}`;
+      ref.min.textContent = `${mins}`;
+      ref.secs.textContent = `${secs}`;
+
+      if(time < 0){
+        ref.timer.textContent = "Finish";
+      }
+    }, 1000);
+  }
+
+   pad(value) {
+    return String(value).padStart(2, "0");
+  }
 
 
 }
 
 
-
-
-
-
-
-
 new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2019'),
-});
+}).updateClockface();
 
-
-
-
-
-// let date = new Date('Oct 01 2020 00:00:00').getTime();
-// let p = document.querySelector('.eventday');
-
-// let currentTime = new Date().getTime();
-// console.log(currentTime);
-
-// let expirationDate = setInterval(() => {
-//   let currentTime = new Date().getTime()
-//   let distance = date - currentTime;
-
-//   let day = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   let hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-//   let minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
-//   let seconds = Math.floor(distance %(1000 * 60) / (1000));
-
-//   p.textContent = `${day}d, ${hours}h, ${minutes}min, ${seconds}sec`;
-
-//   if(distance < 0){
-//     clearInterval(expirationDate);
-//     p.textContent = 'Потрачено'
-//   };
-// }, 1000);
-
-
-
-
-
-
-
-// /*
-//  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-//  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-//  */
-// const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-// /*
-//  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-//  * остатка % и делим его на количество миллисекунд в одном часе
-//  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-//  */
-// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-// /*
-//  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-//  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-//  */
-// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-// /*
-//  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-//  * миллисекунд в одной секунде (1000)
-//  */
-// const secs = Math.floor((time % (1000 * 60)) / 1000);
 
